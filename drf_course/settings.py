62c7375579ec6c73c7566b0322d5626a742c69d2
thiagogbarbosa <tthiagogb@gmail.com>
2026-01-28 11:27:43 -0300

    Creating Models and DB

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'api.User'
