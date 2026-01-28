bb9899a3b935eb7c5cc2dcf002ef5d5327bbf0e3
thiagogbarbosa <tthiagogb@gmail.com>
2026-01-28 15:52:29 -0300

    Adding generic serializers

--- api/serializers.py
+++ api/serializers.py
@@ -50,4 +50,9 @@ class OrderSerializer(serializers.ModelSerializer):
             'total_price'
         )
         
-        
+
+class ProductInfoSerializer(serializers.Serializer):
+    #get all products, count of products, max price 
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer, OrderSerializer
+from django.db.models import Max
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -24,5 +25,16 @@ def product_detail(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': len(products),
+        'max_price':  products.aggregate(max_price=Max('price'))['max_price']
+    })
     
     return Response(serializer.data)
