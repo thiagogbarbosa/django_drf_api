aba1ba3df811d269763784c3d9b8fe4f0fa5d49e
thiagogbarbosa <tthiagogb@gmail.com>
2026-06-01 19:11:33 -0300

    Refactoring views from functions to Generic views (Class based)

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list),
+    path('products/', views.ProductListApiView.as_view()),
     path('products/info/', views.product_info),
-    path('products/<int:pk>/', views.product_detail),
-    path('orders/', views.order_list),
+    path('products/<int:pk>/', views.ProductDetailApiView.as_view()),
+    path('orders/', views.OrderListApiView.as_view()),
 
 ]
