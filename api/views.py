3eb891b816c7eda73147a6c8e2fcf8b72296eae3
thiagogbarbosa <tthiagogb@gmail.com>
2026-06-02 11:01:33 -0300

    Add: REST Client and Customising Permissions

--- api/views.py
+++ api/views.py
@@ -6,12 +6,20 @@ from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.views import APIView
 
+
+#GET and POST requests for Products
 class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 class ProductDetailApiView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
