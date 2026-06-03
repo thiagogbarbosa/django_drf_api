e330c0c9aaedebccadcca6110063314cf69373c4
thiagogbarbosa <tthiagogb@gmail.com>
2026-06-02 22:35:34 -0300

    Adding pagination

--- api/views.py
+++ api/views.py
@@ -9,23 +9,31 @@ from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 from api.filters import ProductFilter, InStockFilterBackend
 
 #GET and POST requests for Products
 class ProductListCreateApiView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+    queryset = Product.objects.order_by('pk')
     serializer_class = ProductSerializer
     filterset_class = ProductFilter
+    
     filter_backends = [
         DjangoFilterBackend,
         filters.SearchFilter,
         filters.OrderingFilter,
         InStockFilterBackend
         ]
+    
     # the '=' is to return the exact match in the field
     search_fields = ['=name','description']
     ordering_fields = ['name', 'price', 'stock']
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 2
+    pagination_class.page_query_param = 'page' 
+    pagination_class.page_size_query_param = 'size'
+    pagination_class.max_page_size = 6
     
     def get_permissions(self):
         self.permission_classes = [AllowAny]
